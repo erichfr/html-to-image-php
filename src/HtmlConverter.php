fea75adb8dcff0f4e3544b8e745ba5c3ce93bb05
Erich Fournier <erichfr1@hotmail.com>
2026-02-26 14:15:16 -0300

    Fix: Add chrome flags for better stability on Windows_2

--- src/HtmlConverter.php
+++ src/HtmlConverter.php
@@ -29,32 +29,40 @@ class HtmlConverter
 
     public function save(string $path): void
     {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            @exec('taskkill /F /IM chrome.exe /T');
+        }
+
         $browserFactory = new BrowserFactory();
         
         $browser = $browserFactory->createBrowser([
             'windowSize' => [$this->width, $this->height],
-            'enableImages' => true,
             'noSandbox' => true,
             'customFlags' => [
-                '--disable-gpu',                
-                '--disable-software-rasterizer', 
-                '--no-sandbox',                
-                '--disable-dev-shm-usage',     
-                '--remote-debugging-port=9222',  
-                '--disable-setuid-sandbox'      
+                '--disable-gpu',
+                '--disable-software-rasterizer',
+                '--remote-debugging-port=9222',
+                '--disable-dev-shm-usage',
+                '--no-first-run',
+                '--no-default-browser-check'
             ]
         ]);
 
         try {
+            usleep(1000000);
+
             $page = $browser->createPage();
             $page->setHtml($this->html);
             
-            usleep(500000); 
+            usleep(800000); 
 
             $page->screenshot([
                 'format'  => 'jpeg',
-                'quality' => 95, 
+                'quality' => 95,
             ])->saveToFile($path);
+
+        } catch (\Exception $e) {
+            throw new \Exception("Falha na renderização: " . $e->getMessage());
         } finally {
             $browser->close();
         }
